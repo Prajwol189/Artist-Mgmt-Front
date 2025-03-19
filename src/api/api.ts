24dc0402cc9fdc9f6ffa0dfa3d64e245e6fc6266
Prajwol189 <mprajwol518@gmail.com>
2025-03-19 15:48:19 +0545

    feat:UI signup

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,26 +2,24 @@ import axios from "axios";
 
 const API_BASE_URL = "http://127.0.0.1:8000/api/users";
 
-// SignUp API
 export const signUp = async (userData: {
   first_name: string;
   last_name: string;
   email: string;
   password: string;
-  phone: string;
-  dob: string;
-  gender: string;
-  address: string;
-  role: string;
+  phone?: string;
+  dob?: string;
+  gender: "m" | "f" | "o";
+  address?: string;
+  role_type: "super_admin" | "artist_manager" | "artist";
 }) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/register/`, userData);
+    const response = await axios.post(`${API_BASE_URL}/signup/`, userData);
     return response.data;
   } catch (error: any) {
     throw error.response ? error.response.data : error.message;
   }
 };
-
 // Login API
 export const login = async (credentials: {
   email: string;
